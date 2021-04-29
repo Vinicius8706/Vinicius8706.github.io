b7a4be4f6d4705341eeca460716d8fb723d40168
Vinicius Farias <fsvinicius9@gmail.com>
2021-04-29 15:13:22 -0300

    site

---
+++ animais-babel/js/modules/scroll-suave.js
@@ -0,0 +1,19 @@
+export default function initScrollSuave() {
+  const linksInternos = document.querySelectorAll('[data-menu="suave"] a[href^="#"]');
+
+  function scrollToSection(event) {
+    event.preventDefault();
+    console.log(event.currentTarget);
+    const href = event.currentTarget.getAttribute('href');
+    const section = document.querySelector(href);
+    console.log(href);
+    section.scrollIntoView({
+      behavior: 'smooth',
+      block: 'start',
+    });
+  }
+
+  linksInternos.forEach((link) => {
+    link.addEventListener('click', scrollToSection);
+  });
+}
---
+++ animais-babel/js/script.js
@@ -0,0 +1,25 @@
+// import $ from 'jquery';
+// import _ from 'lodash';
+import initScrollSuave from './modules/scroll-suave.js';
+import initAnimacaoScroll from './modules/scroll-animacao.js';
+import initAccordion from './modules/accordion.js';
+import initTabNav from './modules/tabnav.js';
+import initModal from './modules/modal.js';
+import initToolTip from './modules/tooltip.js';
+import initDropdownMenu from './modules/dropdown-menu.js';
+import initmenuMobile from './modules/menu-mobile.js';
+import initFuncionamento from './modules/funcionamentos.js';
+import initFetchAnimais from './modules/fetch-animais.js';
+import initFetchBitcoin from './modules/fetch-bitcoin.js';
+
+initScrollSuave();
+initAnimacaoScroll();
+initAccordion();
+initTabNav();
+initModal();
+initToolTip();
+initDropdownMenu();
+initmenuMobile();
+initFuncionamento();
+initFetchAnimais();
+initFetchBitcoin();
